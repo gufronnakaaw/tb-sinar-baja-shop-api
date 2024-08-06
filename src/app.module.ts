b531aeb57c10c8ebb73ca12ca7edbff1f9cc23cc
gufronnakaaw <gufronnakaaw@gmail.com>
2024-08-07 00:09:02 +0700

    add static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { HttpModule } from '@nestjs/axios';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BannersModule } from './banners/banners.module';
@@ -19,10 +21,10 @@ import { PrismaService } from './utils/services/prisma.service';
         expiresIn: '12h',
       },
     }),
-    // ServeStaticModule.forRoot({
-    //   rootPath: join(__dirname, '..', 'public'),
-    //   serveRoot: '/public',
-    // }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/public',
+    }),
     HttpModule,
     BannersModule,
     CategoriesModule,
